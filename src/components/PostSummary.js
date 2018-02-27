31322b74c0d754134a7489a6c8059142355dc72f
Doug Brown <doug.brown@academy.com>
2018-02-27 15:49:16 -0600

    feat: Add basic styling to posts list display

--- src/components/PostSummary.js
+++ src/components/PostSummary.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function PostSummary({
-  post
-}) {
+export default function PostSummary({ post }) {
   return (
     <div className="post-summary">
-      <div className="post-author">{post.author}</div>
-      <div className="post-title">{post.title}</div>
-      <div className="post-body">{post.body}</div>
-      <div className="post-comment-count">{post.commentCount}</div>
-      <div className="post-vote-score">{post.voteScore}</div>
+      <div className="post-summary-line-1">
+        <div className="post-title">{post.title}</div>
+        <div className="post-author">{post.author}</div>
+      </div>
+      <div className="post-summary-line-2">
+        <div className="post-body">{post.body}</div>
+      </div>
+      <div className="post-summary-line-3">
+        <div className="post-comment-count">{`Comments: ${post.commentCount}`}</div>
+        <div className="post-vote-score">{post.voteScore}</div>
+      </div>
     </div>
   );
 }
