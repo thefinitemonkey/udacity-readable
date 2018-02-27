d52869ecc4e11942fd5f82fdc00f763a9f71e786
Doug Brown <doug.brown@academy.com>
2018-02-27 13:28:20 -0600

    feat: Add Provider and connect store to application

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import "./index.css";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 import store from "./store/store.js";
 
-ReactDOM.render(<App store={store} />, document.getElementById("root"));
-registerServiceWorker();
+
+ReactDOM.render(
+  <Provider store={store()}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
+//registerServiceWorker();
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,18 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/root_reducer";
 
-const store = () => createStore(rootReducer, applyMiddleware(thunk));
+
+const logger = store => next => action => {
+    console.group(action.type);
+    console.info('dispatching', action);
+    let result = next(action);
+    console.log('next state', store.getState());
+    console.groupEnd(action.type);
+    return result;
+}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = () => createStore(rootReducer, applyMiddleware(thunk, logger));
 
 export default store;
